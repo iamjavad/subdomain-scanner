20e77178a581202ced11540afb0826f998a89ee6
javad pournosrat <pournosrat.javad@gmail.com>
2021-09-20 00:24:23 +0430

    get response

---
+++ run.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import requests
+
+domain = input(str("Domain: "))
+
+file = open("subdomain.txt")
+
+content = file.read()
+
+subdomains = content.splitlines()
+
+discovered_subdomains = []
+
+for subdomain in subdomains:
+	url = f"http://{subdomain}.{domain}"
+	try:
+		requests.get(url)
+	except requests.ConnectionError:
+		pass
+	else:
+		print("{+} Discoverd subdomain", url)
+		discovered_subdomains.append(url)
+with open("discovered_subdomains.txt", "w") as f:
+	for subdomain in discovered_subdomains:
+		print(subdomain, file=f)
